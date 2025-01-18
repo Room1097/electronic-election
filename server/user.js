c8ea4adef65ef16582cc1e5257ba7fd470241817
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2025-01-18 15:06:15 +0530

    removing logos

--- server/user.js
+++ server/user.js
@@ -1,5 +1,9 @@
 const validUsers = [
     { name: "A", id: "1", voteCasted: false },
+    { name: "B", id: "2", voteCasted: false },
+    { name: "C", id: "3", voteCasted: false },
+    { name: "D", id: "4", voteCasted: false },
+
   ];
   
   module.exports = { validUsers };
